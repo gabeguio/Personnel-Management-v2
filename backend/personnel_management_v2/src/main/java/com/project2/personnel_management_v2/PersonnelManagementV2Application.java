cdc979351913ad233a365e4e1733f6e63b499652
Luis Garcia Quintero <lgarciaq23@gmail.com>
2024-07-24 22:35:42 -0400

    Added Angular frontend and Spring Boot backend applications

---
+++ backend/personnel_management_v2/src/main/java/com/project2/personnel_management_v2/PersonnelManagementV2Application.java
@@ -0,0 +1,13 @@
+package com.project2.personnel_management_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelManagementV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonnelManagementV2Application.class, args);
+	}
+
+}
---
+++ backend/personnel_management_v2/src/test/java/com/project2/personnel_management_v2/PersonnelManagementV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2.personnel_management_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelManagementV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
